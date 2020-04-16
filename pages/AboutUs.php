382f336eccaf15bcd2d5ffea71a41a80d577a1f2
Tutlegoss <tutlegoss@aol.com>
2020-04-16 08:31:37 -0400

    Update AboutUs.php

--- pages/AboutUs.php
+++ pages/AboutUs.php
@@ -4,7 +4,49 @@
 		<div class="row">
 			<?php include("../inc/leftPanel.inc.php"); ?>
 			
-			
+			<div class="col-12 col-lg-10 mt-3 mb-3" id="about-us">
+				<h3 class="mt-3 ml-3 text-center"> 
+					Hypothetical Website for CS 44106 - WP2 at Kent State University taught by Dr A. Guercio
+				</h3>
+				<table class="table table-striped mt-5">
+					<thead>
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Contribution</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Landen Marchand</td>
+							<td>
+								<ul>
+									<li>Home Page</li>
+									<li>Navigation</li>
+									<li>About Us</li>
+								</ul>
+							</td>
+						</tr>
+						<tr>
+							<td>Brandon Jacobs</td>
+							<td>
+								<ul>
+									<li>--</li>
+									<li>--</li>
+								</ul>
+							</td>
+						</tr>
+						<tr>
+							<td>Michael Partridge</td>
+							<td>
+								<ul>
+									<li>--</li>
+									<li>--</li>
+								</ul>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 
