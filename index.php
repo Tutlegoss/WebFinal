8e598f3f5f5a0772a6cc5ef8eaa84db66f8e22f6
Tutlegoss <tutlegoss@aol.com>
2020-04-17 09:06:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -28,36 +28,33 @@
 						<span class="carousel-control-next-icon" aria-hidden="true"></span>
 					</a>
 				</div>
-				<div class="row mt-3">
+				<div class="row mt-3 mb-3">
 					<div class="col-6 mt-3">		
 						<table class="table mb-0" id="indexRating">
-							<thead>
-								<tr class="text-center">
-									<th colspan="2">Top 10 Images</td>
-								</tr>
-							</thead>
-							<tbody>
-								<?php 
-									$topImages = getTopRated(); 
-									
-									for($i = 0, $j = 1; $i < 10; $i += 2, $j += 2) { 
-										$imageNameLeft  = getImageName($topImages[$i]['ImageID']);
-										$imageNameRight = getImageName($topImages[$j]['ImageID']);
-										echo 	"<tr>
-													<td class='text-center'>
-														$imageNameLeft[Title]
-														<img src='./img/square-medium/$imageNameLeft[Path]' class='d-block m-auto'>
-													    Rating: " . round($topImages[$i]['AVG(Rating)'],1) .
-												"	</td>
-													<td class='text-center'>
-														$imageNameRight[Title]
-														<img src='./img/square-medium/$imageNameRight[Path]' class='d-block m-auto'>
-													    Rating: " . round($topImages[$j]['AVG(Rating)'],1) .
-												"	</td>
-												 </tr>"; 
-									}				
-								?> 					
-							</tbody>
+							<tr class="text-center">
+								<th colspan="2">Top 10 Images</td>
+							</tr>
+
+							<?php 
+								$topImages = getTopRated(); 
+								
+								for($i = 0, $j = 1; $i < 10; $i += 2, $j += 2) { 
+									$imageNameLeft  = getImageName($topImages[$i]['ImageID']);
+									$imageNameRight = getImageName($topImages[$j]['ImageID']);
+									echo 	"<tr>
+												<td class='text-center'>
+													$imageNameLeft[Title]
+													<img src='./img/square-medium/$imageNameLeft[Path]' class='d-block m-auto'>
+													Rating: " . round($topImages[$i]['AVG(Rating)'],1) .
+											"	</td>
+												<td class='text-center'>
+													$imageNameRight[Title]
+													<img src='./img/square-medium/$imageNameRight[Path]' class='d-block m-auto'>
+													Rating: " . round($topImages[$j]['AVG(Rating)'],1) .
+											"	</td>
+											 </tr>"; 
+								}				
+							?> 					
 						</table>	
 					</div>
 					<div class="col-6 mt-3">		
