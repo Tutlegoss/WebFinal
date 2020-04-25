aa7e69e3df49a92de1c115ee1a0619e71e69d749
Tutlegoss <tutlegoss@aol.com>
2020-04-25 05:53:54 -0400

    Update leftPanel.inc.php

--- inc/leftPanel.inc.php
+++ inc/leftPanel.inc.php
@@ -27,7 +27,7 @@
 							$countryNames = getCountryNames();
 							foreach($countryNames as $cn)
 								echo "<li class='list-group-item'>
-										  <a href=#> $cn[CountryName] </a>
+										  <a href='".$dir."pages/Single_CountryCity.php?country=$cn[ISO]'> $cn[CountryName] </a>
 									  </li>";
 						?>					
 					</ul>
@@ -43,7 +43,7 @@
 							$cityNames = getCityNames();
 							foreach($cityNames as $cn)
 								echo "<li class='list-group-item'>
-										  <a href='#'> $cn[AsciiName] </a>
+										  <a href='".$dir."pages/Single_CountryCity.php?city=$cn[CityCode]'> $cn[AsciiName] </a>
 									  </li>";
 						?>					
 					</ul>
