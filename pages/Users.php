0c278178b0bb7596f14ca63732e0d0bd0578bda5
Tutlegoss <tutlegoss@aol.com>
2020-04-25 05:22:47 -0400

    Update Users.php

--- pages/Users.php
+++ pages/Users.php
@@ -4,15 +4,15 @@
 		<div class="row">
 			<?php include("../inc/leftPanel.inc.php"); ?>
 
-			<div class="col-12 col-lg-10 mt-3" id="post">
-				<h1 class="ml-3 text-center"> 
+			<div class="col-12 col-lg-10 mt-3 mb-3" id="post">
+				<h1 class="ml-3 text-center mb-5"> 
 					Full List of Users
 				</h1>
 				
 				<?php
 					$users = getUserList(); 
 					foreach($users as $u) {
-						echo '<div class="row justify-content-center">
+						echo '<div class="row justify-content-center mb-2">
 								  <h2>
 									  <a href="single_user.php?id=' . $u['UID'] . '">
 										  <span>' . $u['FirstName'] . " " . $u['LastName'] . '</span>
