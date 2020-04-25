7550647f67f5d52ccf536b487339cd7900c50aee
Tutlegoss <tutlegoss@aol.com>
2020-04-25 12:51:17 -0400

    Update Single_CountryCity.php

--- pages/Single_CountryCity.php
+++ pages/Single_CountryCity.php
@@ -92,7 +92,7 @@
 		// Initialize and add the map
 		function initMap() {
 			// The location of Uluru
-			var uluru = {lat: 78, lng: 80};
+			var uluru = {lat: 25, lng: 80};
 			// The map, centered at Uluru
 			var map = new google.maps.Map(
 			  document.getElementById('map'), {zoom: 4, center: uluru});
