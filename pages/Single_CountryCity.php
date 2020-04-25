f92ca3b426ce97d10ab4c65438933a7d3217e037
Tutlegoss <tutlegoss@aol.com>
2020-04-25 05:55:49 -0400

    Create Single_CountryCity.php

---
+++ pages/Single_CountryCity.php
@@ -0,0 +1,13 @@
+<?php require_once("../inc/header.inc.php"); ?>
+
+	<div class="container-fluid">
+		<div class="row">
+			<?php include("../inc/leftPanel.inc.php"); ?>
+
+			<div class="col-12 col-lg-10 mt-3 mb-3" id="post">
+				
+			</div>
+		</div>
+	</div>
+	
+<?php require_once("./inc/footer.inc.php"); ?>
