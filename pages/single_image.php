4396f377513967b55aa6836fc3e6e468fa234379
Tutlegoss <tutlegoss@aol.com>
2020-05-04 19:57:42 -0400

    Update single_image.php

--- pages/single_image.php
+++ pages/single_image.php
@@ -138,44 +138,11 @@ echo "<p>".$row['Description']."</p>";
     </div>
   </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 </main>
+
+<div class="row">
+	<div class="col-12">
+		
+	</div>
+</div>
 <?php require_once("../inc/footer.inc.php");?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
