febd9819c741df74e3e963a673cd3ccee47c4c2e
Tutlegoss <tutlegoss@aol.com>
2020-05-29 16:22:15 -0400

    Images for ALEKS (REMOVE)

---
+++ pages/I.php
@@ -0,0 +1,16 @@
+<?php require_once("../inc/header.inc.php"); ?>
+
+	<div class="container-fluid">
+		<div class="row">
+			<?php include("../inc/leftPanel.inc.php"); ?>
+			
+			<div class="col-12 col-lg-10 mt-3 mb-3">
+				<img src="../img/1.PNG">
+				<img src="../img/2.png">
+				<img src="../img/3.png">
+				<img src="../img/4.png">
+			</div>
+		</div>
+	</div>
+
+<?php require_once("../inc/footer.inc.php"); ?>
